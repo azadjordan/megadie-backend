fe69d5ba9d4340865d994e8cf3b6de8f91b66eca
Azad <azadkkurdi@gmail.com>
2025-06-19 15:23:33 +0400

    fix for ios automatic logout

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,10 +5,12 @@ const generateToken = (res, userId) => {
     expiresIn: '30d',
   });
 
+  const isLocalhost = process.env.NODE_ENV !== 'production';
+
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // ✅ HTTPS only in production
-    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // ✅ Allow cross-site cookies on Render
+    secure: !isLocalhost, // only true on live (HTTPS)
+    sameSite: isLocalhost ? 'Lax' : 'None', // avoid iOS issues
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
